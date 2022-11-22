06fccd66251bcdb9d2b5adb77ae6ad46490376a6
DrAndyWest <andrewthomaswest@gmail.com>
2022-11-22 14:56:06 +0000

    Updates joint control from yaml and smoother shutdown of joy node

--- scripts/gimbal_joy.py
+++ scripts/gimbal_joy.py
@@ -4,42 +4,57 @@
 import rospy
 from sensor_msgs.msg import Joy
 from dynamixel_workbench_msgs.srv import DynamixelCommand
-# from yaml import safe_load
+from dynamixel_workbench_msgs.msg import DynamixelState, DynamixelStateList
+from yaml import safe_load
 
 
 class gimbaljoyClass():
     def __init__(self):
-        dynamixel_service = "collimated_gimbal/dynamixel_command"
+        self.dynamixel_service = "collimated_gimbal/dynamixel_command"
+        self.dynamixel_state_topic = "collimated_gimbal/dynamixel_state"
 
-        self.rate = rospy.Rate(10) # 2 Hz
+        rospy.loginfo("Waiting for Service: %s", self.dynamixel_service)
+        rospy.wait_for_service(self.dynamixel_service)
+        rospy.loginfo("Service acquired")
+        self.dynamixel_caller = rospy.ServiceProxy(self.dynamixel_service, DynamixelCommand, persistent=True)
+
+        self.dynamixelStateSub = rospy.Subscriber(self.dynamixel_state_topic, DynamixelStateList, self.movingCallback)
+
+        dynamixel_info_file = rospy.get_param('dynamixel_info')
+        self.params = self.parseYaml(dynamixel_info_file)
+
+        self.rate = rospy.Rate(10) # 10 Hz
 
         self.sub_topic_name = 'joy'
         self.joy_deadman = 1 # Circle button on PS4 controller
         self.joy_x_axis = 3  # Used for Yaw
         self.joy_y_axis = 4  # Used for Pitch
 
-        self.id_roll = 83
-        self.id_pitch = 82
-        self.id_yaw = 81
 
-        self.enabled_roll = False
-        self.enabled_pitch = True
-        self.enabled_yaw = False
+        self.enabled_roll = 'roll' in self.params
+        self.enabled_pitch = 'pitch' in self.params
+        self.enabled_yaw = 'yaw' in self.params
+
+        
+        self.id_roll = self.params['roll']['ID'] if self.enabled_roll else 0    # Get motor ID, else set to zero
+        self.id_pitch = self.params['pitch']['ID'] if self.enabled_pitch else 0 # Get motor ID, else set to zero
+        self.id_yaw = self.params['yaw']['ID'] if self.enabled_yaw else 0       # Get motor ID, else set to zero
+
 
         self.limit_angle_roll = 90.0  # +/- degrees
         self.limit_angle_pitch = 45.0 # +/- degrees
-        self.limit_angle_yaw = 90.0   # +/- degrees
+        self.limit_angle_yaw = 120.0   # +/- degrees
 
+        self.isMoving = {}
+        for key in self.params.keys():
+            self.isMoving[key] = False
+
+        self.msgReceived = False
 
         # NEED TO READ YAML TO GET DEFAULTS FOR IDS AND CONTROL TABLE
         self.goal_roll = 0
         self.goal_pitch = 0
         self.goal_yaw = 0
-
-        rospy.loginfo("Waiting for Service: %s", dynamixel_service)
-        rospy.wait_for_service(dynamixel_service)
-        rospy.loginfo("Service acquired")
-        self.dynamixel_caller = rospy.ServiceProxy(dynamixel_service, DynamixelCommand, persistent=True)
 
         rospy.on_shutdown(self.shutdownhook) # Custom Shutdown Hook
 
@@ -47,8 +62,6 @@ class gimbaljoyClass():
 
         # Initialise Joints
         self.initJoints()
-
-        rospy.sleep(5) # Sleeps for 5 sec to ensure the joints are initialised
 
         rospy.Subscriber(self.sub_topic_name, Joy, self.joycallback)
 
@@ -80,41 +93,36 @@ class gimbaljoyClass():
 
     def initJoints(self, home=False):
         self.torqueEnable(True)
-        rospy.sleep(1)
+        # rospy.sleep(1)
         self.safetylimits(True)
-        rospy.sleep(1)
+        # rospy.sleep(1)
 
-        # Roll
-        if self.enabled_roll:
-            self.dynamixel_caller("", self.id_roll, "Goal_Position", 2048)
-
-        # Pitch
-        if self.enabled_pitch:
-            if home:
-                self.dynamixel_caller("", self.id_pitch, "Goal_Position", 1024)
-            else:
-                self.dynamixel_caller("", self.id_pitch, "Goal_Position", 2048)
-        
-        # Yaw
-        if self.enabled_yaw:
-            self.dynamixel_caller("", self.id_yaw, "Goal_Position", 2048)
+        for key in self.params:
+            # 2048 is the center position
+            goal_int = 1024 if key == 'pitch' and home else 2048
+            goal_name = 'home' if home else 'ready'
+            try:
+                result = self.dynamixel_caller("", self.params[key]['ID'], "Goal_Position", goal_int)
+                holdformsg = rospy.wait_for_message(self.dynamixel_state_topic, DynamixelStateList, timeout=1)
+                rospy.sleep(0.2)
+                while True:
+                    if not self.isMoving[key]:
+                        rospy.loginfo("%s move to %s position: completed", key, goal_name)
+                        break
+            except rospy.ServiceException as e:
+                rospy.logwarn("Init Goal Positions call failed: %s"%e)
 
 
     def jointRequester(self, roll, pitch, yaw):
 
-        goal_roll, goal_pitch, goal_yaw = self.input2encoder(roll, pitch, yaw)
+        goal_dict = self.input2encoder(roll, pitch, yaw)
 
-        # Roll
-        if self.enabled_roll:
-            self.dynamixel_caller("", self.id_roll, "Goal_Position", goal_roll)
+        for key in self.params:
+            try:
+                self.dynamixel_caller("", self.params[key]['ID'], "Goal_Position", goal_dict[key])
+            except rospy.ServiceException as e:
+                rospy.logwarn("Goal Postion call failed: %s"%e)
 
-        # Pitch
-        if self.enabled_pitch:
-            self.dynamixel_caller("", self.id_pitch, "Goal_Position", goal_pitch)
-        
-        # Yaw
-        if self.enabled_yaw:
-            self.dynamixel_caller("", self.id_yaw, "Goal_Position", goal_yaw)
 
     def input2encoder(self, roll, pitch, yaw):
         # Convert -1 to 1 into 0-4095 for the Dynamixel goal positions
@@ -129,71 +137,58 @@ class gimbaljoyClass():
         # Yaw
         out_yaw =   2048 * (1 + (yaw   * self.limit_angle_yaw  )/180.0)
 
-        return int(out_roll), int(out_pitch), int(out_yaw)
+        output_dict = {'roll': int(out_roll), 'pitch': int(out_pitch), 'yaw': int(out_yaw)}
+
+        return output_dict
 
     def shutdownhook(self):
         rospy.loginfo("Shutdown Call Received")
         rospy.loginfo("Requesting home positions")
-        self.initJoints(home=True) # Return joints to initial "home" positions RPY
-        rospy.sleep(5)  # Ensure the motors have got to home positions
-        self.torqueEnable(False)
-        rospy.sleep(1)        
+        self.initJoints(home=True) # Return joints to initial "home" positions RPYposition
+        self.torqueEnable(False)  # Disable the motors
+        rospy.sleep(5)
         rospy.loginfo("Shutting Down")
 
 
     def safetylimits(self, enabled):
-        if enabled:
-            # Roll
-            if self.enabled_roll:
-                self.dynamixel_caller("", self.id_roll, "Profile_Acceleration", 1000)
 
-            # Pitch
-            if self.enabled_pitch:
-                self.dynamixel_caller("", self.id_pitch, "Profile_Acceleration", 1000)
-            
-            # Yaw
-            if self.enabled_yaw:
-                self.dynamixel_caller("", self.id_yaw, "Profile_Acceleration", 1000)
+        for key in self.params:
+            result = False
+            profile_accel = 1000 if enabled else 4000
+            try:
+                result = self.dynamixel_caller("", self.params[key]['ID'], "Profile_Acceleration", profile_accel) # Should return True
+                rospy.sleep(0.1)
+                rospy.loginfo("%s joint accel limit: %d", key, profile_accel)
+            except rospy.ServiceException as e:
+                rospy.logwarn("Profile Acceleration call failed: %s"%e)
 
-        else:
-            # Roll
-            if self.enabled_roll:
-                self.dynamixel_caller("", self.id_roll, "Profile_Acceleration", 4000)
-
-            # Pitch
-            if self.enabled_pitch:
-                self.dynamixel_caller("", self.id_pitch, "Profile_Acceleration", 4000)
-            
-            # Yaw
-            if self.enabled_yaw:
-                self.dynamixel_caller("", self.id_yaw, "Profile_Acceleration", 4000)
 
     def torqueEnable(self,enabled):
-        if enabled:
-            # Roll
-            if self.enabled_roll:
-                self.dynamixel_caller("", self.id_roll,  "Torque_Enable", 1)
 
-            # Pitch
-            if self.enabled_pitch:
-                self.dynamixel_caller("", self.id_pitch, "Torque_Enable", 1)
-            
-            # Yaw
-            if self.enabled_yaw:
-                self.dynamixel_caller("", self.id_yaw,   "Torque_Enable", 1)
+        for key in self.params:
+            result = False
+            torque_enable = 1 if enabled else 0
+            try:
+                result = self.dynamixel_caller("", self.params[key]['ID'], "Torque_Enable", torque_enable) # Should return True
+                rospy.sleep(0.1)
+                rospy.loginfo("%s torque: %d", key, enabled)
+            except:
+                rospy.logwarn("Torque call failed: %s"%e)
 
-        else:
-            # Roll
-            if self.enabled_roll:
-                self.dynamixel_caller("", self.id_roll,  "Torque_Enable", 0)
 
-            # Pitch
-            if self.enabled_pitch:
-                self.dynamixel_caller("", self.id_pitch, "Torque_Enable", 0)
-            
-            # Yaw
-            if self.enabled_yaw:
-                self.dynamixel_caller("", self.id_yaw,   "Torque_Enable", 0)
+    def parseYaml(self, filename):
+        f = open(filename, 'r')
+        yamlfile = safe_load(f)
+        f.close()
+        return yamlfile
+
+    def movingCallback(self, dynamixel_state_list):
+        state_list = dynamixel_state_list.dynamixel_state
+        for state in state_list:
+            if state.present_velocity != 0:
+                self.isMoving[state.name] = True
+            else:
+                self.isMoving[state.name] = False
 
 
 
